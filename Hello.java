769d4d0fee0ba2318c98c5fbaea91975dede26de
nikutayade76-maker <nikutayade76@gmail.com>
2026-03-09 17:13:31 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello from GitHub Repository");
+         System.out.println("Program Execute by Jenkins......");
+    }
+}
